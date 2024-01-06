2f702dbebd416ad5f3910859836ab8d4e1a3f3fc
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-01-06 10:46:45 +0530

    setup organizations

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,30 @@
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   publicRoutes: ['/'],
+  afterAuth(auth, req) {
+    if (auth.userId && auth.isPublicRoute) {
+      let path = '/select-org';
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelectionUrl = new URL(path, req.url);
+
+      return NextResponse.redirect(orgSelectionUrl);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== 'select-org') {
+      const orgSelectionUrl = new URL('/select-org', req.url);
+      return NextResponse.redirect(orgSelectionUrl);
+    }
+  },
 });
 
 export const config = {
