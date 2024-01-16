c637aad7a67f29a550ce237978abc71c31369933
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-01-16 16:15:49 +0530

    fix footer

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -4,15 +4,18 @@ import { Button } from '@/components/ui/button';
 const Footer = () => {
   return (
     <div className='fixed bottom-0 w-full p-4 border-t bg-slate-100'>
-      <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
+      <div className='md:max-w-screen-2xl relative mx-auto flex justify-center md:justify-between items-center w-full'>
         <Logo />
-        <div className='space-x-4 md:block md:w-auto flex items-center justify-between w-full'>
-          <Button size='sm' variant='ghost'>
-            Privacy Policy
-          </Button>
-          <Button size='sm' variant='ghost'>
-            Terms of Service
-          </Button>
+        <div className='text-center flex justify-center items-center text-neutral-600'>
+          <span>
+            Developed by{' '}
+            <a
+              href='mailto:devsaurabhsingh@gmail.com'
+              className='font-semibold ml-0 text-neutral-800 underline'
+            >
+              Saurabh Singh
+            </a>
+          </span>
         </div>
       </div>
     </div>
